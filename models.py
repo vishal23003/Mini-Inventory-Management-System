40399e7efabe2499e4ffc99642ec61abd2f01367
Vishal <vishal.s.wagh@ges-coengg.org>
2025-08-15 21:37:52 +0530

    Added models.py file

---
+++ models.py
@@ -0,0 +1,35 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class Supplier(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    contact = db.Column(db.String(120))
+    products = db.relationship('Product', back_populates='supplier')
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    sku = db.Column(db.String(100), unique=True, nullable=False)
+    price = db.Column(db.Float, default=0.0)
+    quantity = db.Column(db.Integer, default=0)
+    reorder_level = db.Column(db.Integer, default=0)
+    supplier_id = db.Column(db.Integer, db.ForeignKey('supplier.id'), nullable=True)
+    supplier = db.relationship('Supplier', back_populates='products')
+    transactions = db.relationship('Transaction', back_populates='product')
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    type = db.Column(db.String(20))  # 'purchase' or 'sale'
+    quantity = db.Column(db.Integer, default=0)
+    unit_price = db.Column(db.Float, default=0.0)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    product = db.relationship('Product', back_populates='transactions')
+
+def init_db(app):
+    with app.app_context():
+        db.init_app(app)
+        db.create_all()
